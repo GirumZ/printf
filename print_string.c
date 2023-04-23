6fdd5315edaf957cecd3c373bfaa0f0077c30e5a
GirumZ <girum.zebe@gmail.com>
2023-04-23 05:25:40 -0700

    print_string

---
+++ print_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_string - prints a string
+ * @args: variable arguments
+ * Return: number of characters writen
+ */
+
+int print_string(va_list args)
+{
+	char *str;
+
+	str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "";
+	return (write(1, str, _strlen(str)));
+}
