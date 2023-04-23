0463eb731d7a124f1967fe59984b6f7cf77c336c
GirumZ <girum.zebe@gmail.com>
2023-04-23 13:05:20 -0700

    pritf

--- _printf.c
+++ _printf.c
@@ -36,10 +36,7 @@ int _printf(const char *format, ...)
 						writ_char += selected_fn(args);
 						i++;
 					}
-					if (selected_fn == NULL)
-					{
-						if (format[i + 1] == ' ' && !format[i + 2])
-							return (-1);
+					return (-1);
 				}
 				continue;
 			}
