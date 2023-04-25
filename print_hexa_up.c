9ad23999973ad918d6c9cc02507ed36e8539da1b
GirumZ <girum.zebe@gmail.com>
2023-04-25 10:29:21 -0700

    print_hexa_up

---
+++ print_hexa_up.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_hexa_up - prints a number in hexadecimal uppercase format
+ * @args: variable argument
+ * Return: number of charaacters printed
+ */
+
+int print_hexa_up(va_list args)
+{
+	int i, j, byte, num, num2, count = 0;
+	char *rev_hexaup;
+
+	num = va_arg(args, int);
+	num2 = num;
+	if (num == 0)
+	{
+		write(1, "0", 1);
+		count = 1;
+	}
+	for (i = 0 ; num > 0 ; i++)
+	{
+		num /= 16;
+		byte++;
+	}
+	rev_hexaup = malloc(sizeof(char) * byte);
+	if (rev_hexaup == NULL)
+		return (-1);
+	for (i = 0 ; num2 > 0 ; i++)
+	{
+		if ((num2 % 16) < 10)
+			rev_hexaup[i] = (num2 % 16) + '0';
+		if ((num2 % 16) >= 10)
+			rev_hexaup[i] = (num2 % 16) - 10 + 'A';
+		num2 /= 16;
+	}
+	for (j = i ; j >= 0 ; j--)
+	{
+		write(1, &rev_hexaup[j], 1);
+		count++;
+	}
+	free(rev_hexaup);
+	return (count);
+}
