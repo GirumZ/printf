1c637ebf568239f213f2b5cfb4fc12d494fe2b36
GirumZ <girum.zebe@gmail.com>
2023-04-25 00:54:17 -0700

    strlen

---
+++ strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - This function calculate the length of a string
+ * @s: is the string to be measured
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0 ; *(s + count) != '\0' ; count++)
+	{
+	}
+	return (count);
+}
